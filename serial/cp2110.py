663b4eca0a0e4d78ebdff71e802d2547cefe97e4
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2019-01-29 11:55:18 +0000

    Simplify read loop.

--- serial/cp2110.py
+++ serial/cp2110.py
@@ -247,16 +247,11 @@ class Serial(SerialBase):
     def _hid_read_loop(self):
         try:
             while self.is_open:
-                try:
-                    data = self._hid_handle.read(64, timeout_ms=100)
-                    if not data:
-                        continue
-                    data_len = data.pop(0)
-                    assert data_len == len(data)
-                    self._read_buffer.put(bytearray(data))
-                except:
-                    break
+                data = self._hid_handle.read(64, timeout_ms=100)
                 if not data:
-                    self._read_buffer.put(None)
+                    continue
+                data_len = data.pop(0)
+                assert data_len == len(data)
+                self._read_buffer.put(bytearray(data))
         finally:
             self._thread = None
